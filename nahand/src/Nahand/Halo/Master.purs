7580347bff91ec324e21d180f6b3451a09bc168f
Thomas Letan <leth@leth.io>
2016-09-22 15:26:31 +0200

    clean up the code

--- nahand/src/Nahand/Halo/Master.purs
+++ nahand/src/Nahand/Halo/Master.purs
@@ -7,21 +7,21 @@ module Nahand.Halo.Master
   , MasterEff
   ) where
 
-import Servant.Auth.Token.Api (PostTokenRefreshReq(..))
-import Auth.Token (AccessGrant(..), EphemeralToken(..))
 import Prelude
-import Control.Monad.Aff (Aff, later')
-import Network.HTTP.Affjax (AJAX)
-import Data.Maybe (Maybe(..), isNothing)
-import Data.Either (Either(..))
-import Data.Monoid (append)
-import Control.Monad.Eff.Console (log, CONSOLE)
-import Data.Functor.Coproduct  (Coproduct)
-import Halogen                 (query, action, request, fromAff, ParentHTML, ParentDSL, ChildF(..), ParentState, Component, parentComponent, modify)
-import Halogen.HTML.Indexed    (h1_, text, div_, slot)
-import Nahand.Halo.LogIn       (LoginEff, login)
-import Nahand.Halo.LogIn       (State, Query(..), initialState) as LogIn
-import Nahand.Halo.Base        (runBzEffect, settings)
+
+import Servant.Auth.Token.Api    (PostTokenRefreshReq(..))
+import Auth.Token                (AccessGrant(..), EphemeralToken(..))
+import Control.Monad.Aff         (Aff, later')
+import Network.HTTP.Affjax       (AJAX)
+import Data.Maybe                (Maybe(..), isNothing)
+import Data.Either               (Either(..))
+import Control.Monad.Eff.Console (CONSOLE)
+import Data.Functor.Coproduct    (Coproduct)
+import Halogen                   (query, request, fromAff, ParentHTML, ParentDSL, ChildF(..), ParentState, Component, parentComponent, modify)
+import Halogen.HTML.Indexed      (h1_, text, div_, slot)
+import Nahand.Halo.LogIn         (LoginEff, login)
+import Nahand.Halo.LogIn         (State, Query(..), initialState) as LogIn
+import Nahand.Halo.Base          (runBzEffect, settings)
 
 import Renaissance.Api.Bz.Data (TokenGetRouteBody(..))
 import GenBzApi (postTokenGet, postTokenRefresh)
@@ -35,6 +35,8 @@ derive instance eqUnit :: Eq LogInSlot
 derive instance ordUnit :: Ord LogInSlot
 
 type MasterEff = LoginEff (console :: CONSOLE, ajax :: AJAX)
+type MasterHTML = ParentHTML LogIn.State Query LogIn.Query (Aff MasterEff) LogInSlot
+type MasterDSL = ParentDSL State LogIn.State Query LogIn.Query (Aff MasterEff) LogInSlot
 
 type State' = ParentState State LogIn.State Query LogIn.Query (Aff MasterEff) LogInSlot
 type Query' = Coproduct Query (ChildF LogInSlot LogIn.Query)
@@ -56,7 +58,7 @@ master :: Component State' Query' (Aff MasterEff)
 master = parentComponent { render, eval, peek: Just peek }
   where
     render :: State
-            -> ParentHTML LogIn.State Query LogIn.Query (Aff MasterEff) LogInSlot
+            -> MasterHTML
     render o = if isNothing o.connected
                then div_ [ h1_ [ text "Hi" ]
                          , slot LogInSlot (\_ -> { component: login
@@ -66,36 +68,35 @@ master = parentComponent { render, eval, peek: Just peek }
                 else div_ [ text "connected" ]
     
     eval :: Query
-         ~> ParentDSL State LogIn.State Query LogIn.Query (Aff MasterEff) LogInSlot
+         ~> MasterDSL
     eval (Sleep next) = pure next
 
     peek :: forall x
           . ChildF LogInSlot LogIn.Query x
-          -> ParentDSL State LogIn.State Query LogIn.Query (Aff MasterEff) LogInSlot Unit
-    peek (ChildF p q) =
-      case q of
-           (LogIn.Submitting _) ->
-               do email <- query p (request LogIn.GetEmail)
-                  res <- askTokenForEmail email
-                  case res of
-                       (Right (Right ag)) -> do modify $ connectionSucceed ag
-                                                waitAndRefresh ag
-                       _ -> modify $ errorMsg "fail to connect"
-           _ ->
-               pure unit
+          -> MasterDSL Unit
+    peek (ChildF p (LogIn.Submitting _)) =
+        do email <- query p (request LogIn.GetEmail)
+           askTokenForEmail email
+    peek _ = pure unit
 
-    askTokenForEmail (Just email) =
-      do let body = TokenGetRouteBody { email: email }
-         res <- fromAff $ runBzEffect settings (postTokenGet body)
-         pure $ Right res
-    askTokenForEmail Nothing = pure $ Left "No email to use"
 
-    waitAndRefresh ag =
-      do let body = PostTokenRefreshReq { refreshToken : unwrapRefreshToken ag }
-         res <- fromAff $ later' 2000 $ runBzEffect settings (postTokenRefresh body)
-         case res of Right ag' -> do modify $ connectionSucceed ag'
-                                     waitAndRefresh ag'
-                     _         -> do modify $ errorMsg "unable to refresh"
+askTokenForEmail :: Maybe String -> MasterDSL Unit
+askTokenForEmail (Just email) =
+  do let body = TokenGetRouteBody { email: email }
+     res <- fromAff $ runBzEffect settings (postTokenGet body)
+     case res of (Right ag) -> do modify $ connectionSucceed ag
+                                  waitAndRefresh ag
+                 _ -> modify $ errorMsg "fail to connect"
+
+askTokenForEmail Nothing = modify $ errorMsg "invalid state"
+
+waitAndRefresh :: AccessGrant -> MasterDSL Unit
+waitAndRefresh ag =
+  do let body = PostTokenRefreshReq { refreshToken : unwrapRefreshToken ag }
+     res <- fromAff $ later' 2000 $ runBzEffect settings (postTokenRefresh body)
+     case res of Right ag' -> do modify $ connectionSucceed ag'
+                                 waitAndRefresh ag'
+                 _         -> do modify $ errorMsg "unable to refresh"
 
 unwrapRefreshToken :: AccessGrant -> String
 unwrapRefreshToken (AccessGrant o) = case o.refresh of (EphemeralToken o) -> o.value
